0f278aa575d43db58123ca84e54ba97a8ee664ea
noobMasterProgrammer <ghoshabhishek1640@gmail.com>
2020-12-14 01:17:03 +0530

    commit

--- mongo-rest-crud/src/main/java/com/example/demo/models/Pets.java
+++ mongo-rest-crud/src/main/java/com/example/demo/models/Pets.java
@@ -4,8 +4,12 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "pets")
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Document
 public class Pets {
+	
+	@JsonIgnore
 	@Id
 	public ObjectId _id;
 
